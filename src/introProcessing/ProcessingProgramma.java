5d60dd3b0a5f8621e45251f6a036cc40aeb31aad
Sem <sem.van.manen@hotmial.com>
2018-02-23 15:05:09 +0100

    Programma werkt

---
+++ src/introProcessing/ProcessingProgramma.java
@@ -0,0 +1,10 @@
+package introProcessing;
+
+public class ProcessingProgramma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/uitprobeersel/achttien.java
+++
@@ -1,5 +0,0 @@
-package uitprobeersel;
-
-public class achttien {
-
-}
